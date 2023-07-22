0c38bc1d292688e8be17b319cd5bf3c04845b74b
mama679 <ingmariomanotas@hotmail.com>
2023-07-22 17:19:10 -0500

    Clase control

--- app/libs/control.php
+++ app/libs/control.php
@@ -1,12 +1,33 @@
 <?php
 /**
  * Control base manejo de uris y lanza procesos
+ * Primer parametro: Controlador
+ * Segundo parametro: Metodo
+ * Lo siguiente parametros: Parametros o valores
  */
 
  class Control{
     function __construct()
     {
-        print "Bienvenid@ a la Aplicación";
+        //print "Bienvenid@ a la Aplicación";
+        $url = $this->separarURL();
+        var_dump($url);
+    }
+
+    public function separarURL()
+    {
+        $url = "";
+        if(isset($_GET['url']))
+        {
+            //eliminar caracter final
+            $url = rtrim($_GET['url'],"/");
+            $url = rtrim($_GET['url'],"\\");
+            //sanatizamos
+            $url = filter_var($url,FILTER_SANITIZE_URL);
+            $url = explode("/",$url);
+        }
+
+        return $url;
     }
  }
 ?>
